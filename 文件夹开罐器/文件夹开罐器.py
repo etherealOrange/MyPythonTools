47da3dc7a53294d6cab04a7965db551367d52a86
hi world <2607233502@qq.com>
2025-02-13 19:47:26 +0800

    修改了文件夹开罐器，使其可以按照层级开罐

--- 文件夹开罐器/文件夹开罐器.py
+++ 文件夹开罐器/文件夹开罐器.py
@@ -13,19 +13,34 @@ def RenameFileName(name, suffix):
         detectName = os.path.join(root, f"{name}{addStr}.{suffix}")
         repeatsNum += 1
     return detectName
+def RenameDirName(name):
+    detectName = os.path.join(root, f"{name}")
+    repeatsNum = 0
+    while os.path.exists(detectName):
+        addStr = f"-重复 {repeatsNum}"
+        detectName = os.path.join(root, f"{name}{addStr}")
+        repeatsNum += 1
+    return detectName
 
 
-def DFS_Search_MoveFile(folder):
+def DFS_Search_MoveFile(folder,lvl):
+    if lvl == 0:
+        return 0
+    if lvl < -500:
+        lvl = -1
     ls = os.listdir(folder)
     for i in ls:
         realPath = (os.path.join(folder, i))
         if os.path.isfile(realPath):
-            [nosuffix, suffix] = i.rsplit('.', maxsplit=1)
-            name = nosuffix.rsplit('\\', maxsplit=1)[0]
+            [name, suffix] = i.rsplit('.', maxsplit=1)
             OKname = RenameFileName(name, suffix)
             shutil.move(realPath, f"{OKname}")
         if os.path.isdir(realPath):
-            DFS_Search_MoveFile(realPath)
+            if DFS_Search_MoveFile(realPath,lvl-1) == 0:
+                OKname = RenameDirName(i)
+                shutil.move(realPath, f"{OKname}")
+    return -1
+
 
 if __name__ == '__main__':
     ls = os.listdir(startRoot)
@@ -35,5 +50,13 @@ if __name__ == '__main__':
         ls.remove("开罐结果")
     else:
         os.mkdir("开罐结果")
-    for i in ls:
-        DFS_Search_MoveFile(os.path.join(startRoot, i))
+    print("选择你要打开的层级（回车表示无限）：")
+    lvl = input()
+    if len(lvl) == 0 or int(lvl) < 0:
+        for i in ls:
+            DFS_Search_MoveFile(os.path.join(startRoot, i),-1)
+    elif int(lvl) > 0:
+        for i in ls:
+            DFS_Search_MoveFile(os.path.join(startRoot, i),int(lvl))
+
+
