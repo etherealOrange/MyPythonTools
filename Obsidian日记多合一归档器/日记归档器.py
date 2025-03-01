97effa120a255954434addf75a4e7e3d47cef56a
hi world <2607233502@qq.com>
2025-03-01 19:02:59 +0800

    修改了日记归档器的时间判断逻辑，能够根据md内yuml信息进行日期判断，修复了最后输出时可能出现的数组越界，增加了更多的注解

--- Obsidian日记多合一归档器/日记归档器.py
+++ Obsidian日记多合一归档器/日记归档器.py
@@ -4,7 +4,6 @@ from datetime import datetime
 # !!!!!!!!!!修改位置!!!!!!!!!!
 ARCH_src = "ARCH"
 diary_src = "diary"
-Ignore_Month = False
 
 def loadYuml():
     global addI
@@ -13,12 +12,15 @@ def loadYuml():
     global summary
     global feelings
     global mentioned
-
+    IsThisMonth = True
+    ThisMonth = year+"-"+month
     addI = 1
     while i + addI < len(lines) and lines[i + addI] != "---":
         splited = lines[i + addI].split(":", 1)
         if "Date" == splited[0]:
             fileTime = splited[1][1:]
+            if ThisMonth not in fileTime:
+                IsThisMonth = False
         elif "文档类型" == splited[0]:
             DocType = splited[1][1:]
         elif "摘要" == splited[0]:
@@ -40,36 +42,31 @@ def loadYuml():
             input()
             exit(-100)
         addI += 1
-    return addI
+    return addI,IsThisMonth
 
 
 if __name__ == '__main__':
+    # 拿到日记文件夹下全部文件列表
     MdList = os.listdir(diary_src)
     now = datetime.now()
-    year = now.year
+    year = str(now.year)
     month = (now.month - 2) % 12 + 1
+    if month < 10:
+        month = "0" + str(month)
+    else:
+        month = str(month)
+    # 过滤后的文件列表
     filtered_list = []
     for i in MdList:
         if not os.path.isfile(diary_src + "/" + i):
             continue
         if "md" != i.split(".")[-1].lower():
             continue
-        filestat = os.stat(diary_src + "/" + i)
-        createdTime = filestat.st_ctime
-        createdTime = datetime.fromtimestamp(createdTime)
-        if createdTime.year != year:
-            continue
-        if not Ignore_Month and createdTime.month != month:
-            continue
+        # 初选过的md文件
         filtered_list.append(i)
     MdList = filtered_list
     print(MdList)
-    year = str(now.year)
-
-    if month < 10:
-        month = "0" + str(month)
-    else:
-        month = str(month)
+    # 精选过 真正要记录的md的信息
     diarys_info = []
     for j in range(len(MdList)):
         with open(diary_src + "/" + MdList[j], "r", encoding="utf-8") as f:
@@ -92,13 +89,15 @@ if __name__ == '__main__':
             while i < len(lines):
                 if FirstInYuml and "---" == lines[i]:
                     InYuml = False
-                    addI = loadYuml()
+                    addI, isthismonth = loadYuml()
                     i += addI
                 else:
                     if "-" != lines[i]:
                         content.append(lines[i])
                 i += 1
-        diarys_info.append([fileTime, DocType, summary, feelings, mentioned,content])
+            if isthismonth:
+                diarys_info.append([fileTime, DocType, summary, feelings, mentioned,content])
+
     diarys_info.sort(key=lambda x: x[0][:10])
     for i in diarys_info:
         print(i)
@@ -107,7 +106,7 @@ if __name__ == '__main__':
 
     with open(ARCH_src+'/'+year+'-'+month+'日记总结.md','w',encoding="utf-8") as f:
         f.write(YumHead)
-        for j in range(len(MdList)):
+        for j in range(len(diarys_info)):
             CallOutContent = (f"> [!Document]- {diarys_info[j][0]} {diarys_info[j][1]} {diarys_info[j][2]}\n"
                           f"> 心情::{','.join(diarys_info[j][3])}\n"
                           f"> 提及内容::{','.join(diarys_info[j][4])}\n"
