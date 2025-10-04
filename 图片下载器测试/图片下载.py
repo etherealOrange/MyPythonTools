a5a9a0ef405404c4a4e17e70a91054a6d2b8713f
hi world <thisisfake@email.com>
2025-10-04 18:45:19 +0800

    添加entry文件的解析方案 串联文件解析和下载

--- 图片下载器测试/图片下载.py
+++ 图片下载器测试/图片下载.py
@@ -1,8 +1,9 @@
 import os
-
+import json
 import requests
 
 url = "https://img-blog.csdnimg.cn/6804157b46ca4506a9fc382c05eb4e58.png"
+entry = "./entry.json"
 
 def download_image(url, save_folder):
     try:
@@ -19,8 +20,25 @@ def download_image(url, save_folder):
     except Exception as ex:
         print(ex)
 
+def retrieve_cover_utl(json_path):
+    if os.path.exists(json_path):
+        with open(json_path,'r' ,encoding='utf-8') as f:
+            data = json.load(f)
+            if "cover" in data:
+                return data["cover"]
+    return None
+
+
 if __name__ == '__main__':
-    download_image(url,"./")
+    cover_url = retrieve_cover_utl(entry)
+    if cover_url is None:
+        print("Cover url not found")
+        exit(1)
+    else:
+        print("Cover url found")
+        download_image(cover_url, "./")
+
+    # download_image(url,"./")
     # image = requests.get(url).content
     # with open("test.jpg", "wb") as f:
     #     f.write(image)
