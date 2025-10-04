cb572625f5b3ce90da451097dcc6215a4aa7c436
hi world <thisisfake@email.com>
2025-10-04 21:37:30 +0800

    删除了过去的老代码

--- 手机B站缓存整合/手机B站缓存视频整合.py
+++ 手机B站缓存整合/手机B站缓存视频整合.py
@@ -17,15 +17,6 @@ name_folder_dirs = [name_videoM4S, name_audioM4S, name_indexJSON]
 
 def PJoin2(path1, path2):
     return os.path.join(path1, path2)
-def MakeCommand(ALL_Path,output_name):
-    abs_path = os.path.abspath(ALL_Path)
-    return  [
-        "ffmpeg","-loglevel","quiet",
-        "-i", PJoin2(abs_path,name_audioM4S),
-        "-i", PJoin2(abs_path,name_videoM4S),
-        "-c", "copy", "-y",
-        PJoin2(abs_path,output_name + ".mp4"),
-    ]
 
 def generate_mix_audio_video_cover_command(video_path,audio_path,cover_path,out_video_path):
     video_path = os.path.abspath(video_path)
@@ -134,7 +125,6 @@ def do_mix_video(root_path,root_dirs):
         print(ex)
         return
 
-
 def search_dirs(root_path):
     ls = os.listdir(root_path)
     if search_danmaku(ls):
@@ -146,56 +136,8 @@ def search_dirs(root_path):
             if os.path.isdir(path):
                 search_dirs(path)
 
-def Start_Process(path):
-    ls = os.listdir(path)
-    if name_subXML in ls and name_messageJSON in ls:
-        next_folder = ""
-        for i in ls:
-            if os.path.isdir(PJoin2(path,i)):
-                next_folder = PJoin2(path,i)
-                # 请确保你在entry.json的文件夹只有一个，因为默认应该只有一个
-                break
-        if next_folder == "":
-            print("你的文件夹不存在")
-            input()
-            exit(-1)
-        next_ls = os.listdir(next_folder)
-
-        with open(PJoin2(path, name_messageJSON), 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            title = data["title"]
-            page_data = data["page_data"]
-            if "part" in page_data.keys():
-                part = page_data["part"]
-            if part == "":
-                fixed_output_name = part
-            else:
-                fixed_output_name = title
-        if name_audioM4S in next_ls and name_videoM4S in next_ls and name_indexJSON in next_ls:
-            shutil.move(PJoin2(next_folder,name_audioM4S),PJoin2(path,name_audioM4S))
-            shutil.move(PJoin2(next_folder,name_videoM4S),PJoin2(path,name_videoM4S))
-            shutil.move(PJoin2(next_folder,name_indexJSON),PJoin2(path,name_indexJSON))
-            try:
-                sp.run(MakeCommand(path, fixed_output_name), check=True)
-            except sp.CalledProcessError as e:
-                print(e)
-                input()
-                exit(-100)
-            before_folder = os.path.abspath(path).rsplit("\\",1)[0]
-            shutil.move(path,PJoin2(before_folder,fixed_output_name))
-
-    else:
-        for i in ls:
-            if os.path.isdir(PJoin2(path,i)):
-                Start_Process(PJoin2(path,i))
-
 
 if __name__ == '__main__':
-    # first_Folder = os.listdir(root)
-    # for path in first_Folder:
-    #     if os.path.isdir(path):
-    #         Start_Process(path)
-
     search_dirs(root)
     print("结束按任意键退出。")
     input()
