7096e152562d05fa7c4a22572cba0263faace276
hi world <thisisfake@email.com>
2025-10-04 18:17:21 +0800

    将下载方法包装为方法，检测了网页错误，会创建新文件夹，存在同名文件跳过，分块写入，避免一次性把图片全部加载到内存中

--- 图片下载器测试/图片下载.py
+++ 图片下载器测试/图片下载.py
@@ -1,7 +1,26 @@
+import os
+
 import requests
 
-url = "https://img-blog.csdnimg.cn/92fb30f4b5c040a59cb4d82d4a37d41e.png"
-image = requests.get(url).content
+url = "https://img-blog.csdnimg.cn/6804157b46ca4506a9fc382c05eb4e58.png"
 
-with open("test.jpg", "wb") as f:
-    f.write(image)
+def download_image(url, save_folder):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        os.makedirs(save_folder, exist_ok=True)
+        save_path = os.path.join(save_folder, os.path.basename(url))
+        if os.path.exists(save_path):
+            print(save_path+" already exists, skipping...")
+            return
+        with open(save_path, 'wb') as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+    except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__':
+    download_image(url,"./")
+    # image = requests.get(url).content
+    # with open("test.jpg", "wb") as f:
+    #     f.write(image)
